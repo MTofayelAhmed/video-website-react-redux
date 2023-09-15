db025fe08b7c4b8e1b16e2789e0a72822619a058
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-09-15 18:14:45 +0600

    done with componenet structure

---
+++ src/components/VideoList/RelatedVideoLIst.js
@@ -0,0 +1,12 @@
+import RelatedVideoListItem from "./RelatedVideoListItem";
+
+
+const RelatedVideoLIst = () => {
+  return (
+    <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+   <RelatedVideoListItem></RelatedVideoListItem>
+    </div>
+  );
+};
+
+export default RelatedVideoLIst;
